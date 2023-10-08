0d0ec167c3be2ed17933398ec021681f9b40da5e
wendell00 <wendellborges2003@gmail.com>
2023-10-08 17:52:27 -0300

    Arquivos Iniciais

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// var dataAtual = new Date();
+
+// // Obtém o ano atual
+// var anoAtual = dataAtual.getFullYear();
+
+// var elementDate = document.querySelector('.year-date');
+// elementDate.innerText = anoAtual;
